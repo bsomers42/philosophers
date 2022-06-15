7d3382d9a40a4d797f85be3871999d5ddcca2f64
bsomers42 <brittsomers22@gmail.com>
2022-06-15 17:13:23 +0200

    Uitbreiden code met DPP

--- philo.h
+++ philo.h
@@ -6,8 +6,24 @@
 /*   By: bsomers <bsomers@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/11 11:16:28 by bsomers       #+#    #+#                 */
-/*   Updated: 2022/06/11 11:19:03 by bsomers       ########   odam.nl         */
+/*   Updated: 2022/06/15 13:34:02 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+
+# define PHILO_H
+
 #include <pthread.h>
+
+typedef struct s_philo
+{
+	int		philos;
+	int		sleep_sec;
+	int		die_sec;
+    int     eat_sec;
+}		t_philo;
+
+int	ft_atoi(const char *nptr);
+
+#endif
